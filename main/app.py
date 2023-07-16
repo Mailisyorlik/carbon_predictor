439bc73a3c272c92c69a754d0e476277bdb1c950
Liam Kilroy <liamkilroy@gmail.com>
2023-07-15 17:34:34 -0700

    adding json to dataframe as well as actually successfully calling the API

--- main/app.py
+++ main/app.py
@@ -5,8 +5,9 @@ import requests
 import schedule
 import time
 from twilio.rest import Client
-
+from datetime import timedelta, date
 import json
+import pandas as pd
 
 # Read the secrets from the JSON file
 with open('/Users/liamkilroy/Documents/py_projects/carbon_predictor/secrets.json') as file:
@@ -21,8 +22,8 @@ emap_api_password = secrets['password']
 
 # zone = zone/US-NW-NEVP for nv energy customers
 
-# ElectricityMap API endpoint
-api_url = 'https://api-access.electricitymaps.com/free-tier/carbon-intensity/history'
+
+#api_url = 'https://api-access.electricitymaps.com/free-tier/carbon-intensity/history'
 
 
 # Twilio credentials
@@ -31,20 +32,58 @@ auth_token = secrets['auth_token']
 twilio_phone_number = secrets['twilio_phone_number']
 your_phone_number = secrets['your_phone_number']
 
-def fetch_hourly_data(zone='US-NW-NEVP', timestamp='YYYY-MM-DD'):
+
+def fetch_test(api_key) -> str: #eventually put the api key as a called argument
+    url = "https://api-access.electricitymaps.com/free-tier/carbon-intensity/history?zone=US-NW-NEVP" #zone is set for NV energy 
+    headers = {
+  "auth-token": api_key #fix this, they should not be referring to the same thing
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response.text)
+    return response.text
+
+json_response = fetch_test(api_key) #maybe add api key as second argument
+
+
+timestamp = date.today() - timedelta(days = 1)
+
+
+def fetch_hourly_data(zone='US-NW-NEVP', timestamp = timestamp) -> str: #what should this be returning
     url = api_url.format(zone=zone, timestamp=timestamp)
     headers = {'Authorization': api_key}
     response = requests.get(url, headers=headers)
     if response.ok:
         return response.json()
     else:
-        print(f'Error fetching data: {response.status_code} - {response.text}')
+        print(f'Error fetching hourly data: {response.status_code} - {response.text}')
         return None
+#data = fetch_hourly_data(zone='US-NW-NEVP', timestamp = timestamp)
 
 def get_lowest_carbon_intensity_hours(data):
     hourly_carbon_intensity = data['data']['carbonIntensity']
     lowest_hours = sorted(hourly_carbon_intensity, key=lambda x: x['value'])[:2]
+    print(lowest_hours)
     return lowest_hours
+
+
+json_response = fetch_test(api_key) #maybe add api key as second argument
+
+if json_response:
+    df = pd.read_json(json_response)
+    print(df)
+    ci_sorted = df.sort_values(by = 'carbonIntensity')
+    ci_lowest_hours = ci_sorted[['zone', 'history']].head(2)
+    ci_lowest_hours['startTime'] = pd.to_datetime(ci_lowest_hours['history'].apply(lambda x: x['startTime']))
+    ci_lowest_hours.rename(columns = {'startTime':'Date', 'zone':'Zone'}, inplace = True)
+    ci_lowest_hours.drop(columns = 'history', inplace = True)
+    print(ci_lowest_hours)
+#get_lowest_carbon_intensity_hours(nv_dataframe)
+#fetch_hourly_data(zone = 'US-NW-NEVP', timestamp = timestamp)
+#get_lowest_carbon_intensity_hours(response.json)
+
+#if __name__ == "__main__":
+
 
 def get_yesterdays_ci():
     yesterday = '2023-07-12'  # Replace with the actual date in the format YYYY-MM-DD
@@ -55,8 +94,7 @@ def get_yesterdays_ci():
         for hour in lowest_hours:
             print(f"Hour: {hour['startTime']}, Carbon Intensity: {hour['value']}")
         return lowest_hours
-
-get_yesterdays_ci()
+#get_yesterdays_ci()
 # Send SMS using Twilio
 def send_sms(message):
     client = Client(account_sid, auth_token)
@@ -79,6 +117,12 @@ def job():
         message = f"Lowest CI Hours{lowest_hours}"
         print(message)
         send_sms(message)
+
+job()
+def produce_forecast() -> None:
+    '''placeholder function for inference from forecast_models.py'''
+    pass
+
 
 #job()
 
